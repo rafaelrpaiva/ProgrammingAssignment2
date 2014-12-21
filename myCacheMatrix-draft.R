a2746d4076917e9f1679df7b4914c4cd0e93b262
rafaelrpaiva <rafaelpaiva@gmail.com>
2014-12-21 15:41:12 -0200

    Arquivos da disciplina

--- myCacheMatrix-draft.R
+++ myCacheMatrix-draft.R
@@ -1,33 +1,20 @@
-## Function to start and save a matrix in cache, allowing get and set functions
-## as well
-## By rafaelpaiva@gmail.com - a brazilian interested in job offers to US/Canada.
-## Coursera Introduction to R
+## http://stackoverflow.com/questions/23796316/returning-the-inverse-matrix-from-a-cached-object-in-r
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function (x = matrix()) {
     mx <<- NULL
-    
     set <- function (y) {
         x <<- y
         mx <<- NULL
     }
-
     get <- function () x
-
     setmatrix <- function(solve) mx <<- solve
-    
     getmatrix <- function() mx
-    
     list(set=set, get=get,
          setmatrix=setmatrix,
          getmatrix=getmatrix)
-    
 }
 
-
-## Function to calculate the inverse matrix, caching then.
-
-cacheSolve <- function(x, ...) {
-    ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
     mx <- x$getmatrix()
     if(!is.null(mx)) {
         message("getting cached data")
@@ -36,5 +23,5 @@ cacheSolve <- function(x, ...) {
     matrix <- x$get()
     mx <- solve(matrix, ...)
     x$setmatrix(mx)
-    mx   
+    mx
 }
